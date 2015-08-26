0837f4f8b8f34aec3acee91a99052d2e607e4647
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-08-26 20:42:15 +0530

    Design section : Added angular routing and controller view structure. Also added 4 list layout images

---
+++ control/design/app.js
@@ -0,0 +1,28 @@
+'use strict';
+(function (angular, buildfire) {
+  angular
+    .module('vimeoPluginDesign', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'templates/home.html',
+          controllerAs: 'DesignHome',
+          controller: 'DesignHomeCtrl'
+        })
+        .otherwise('/');
+    }])
+    .filter('getImageUrl', function () {
+      return function (url, width, height, type) {
+        if (type == 'resize')
+          return buildfire.imageLib.resizeImage(url, {
+            width: width,
+            height: height
+          });
+        else
+          return buildfire.imageLib.cropImage(url, {
+            width: width,
+            height: height
+          });
+      }
+    });
+})(window.angular, window.buildfire);
---
+++ control/design/controllers/design.home.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function (angular, window) {
+  angular
+    .module('vimeoPluginDesign')
+    .controller('DesignHomeCtrl', ['$scope',
+      function ($scope) {
+
+
+      }]);
+})(window.angular, window);
