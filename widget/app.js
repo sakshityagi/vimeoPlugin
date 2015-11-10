da23471ccaaddca39e2d14292302143cd9791423
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-11-10 16:00:06 +0530

    refactored routing for increasing app performance in widget section. Also fixed back button issue.

--- widget/app.js
+++ widget/app.js
@@ -16,48 +16,12 @@
 
       $routeProvider
         .when('/', {
-          resolve: {
-            videoData: ['DataStore', '$q', 'TAG_NAMES', 'CONTENT_TYPE', 'Location', '$rootScope',
-              function (DataStore, $q, TAG_NAMES, CONTENT_TYPE, Location, $rootScope) {
-                var deferred = $q.defer();
-                var success = function (result) {
-                    if (result.data && result.data.content) {
-                      $rootScope.contentType = result.data.content.type;
-                      if (result.data.content.type && result.data.content.type === CONTENT_TYPE.SINGLE_VIDEO && result.data.content.videoID) {
-                        Location.goTo("#/video/" + result.data.content.videoID);
-                        deferred.resolve();
-                      }
-                      else if (result.data.content.type && ((result.data.content.type === CONTENT_TYPE.CHANNEL_FEED) || (result.data.content.type === CONTENT_TYPE.USER_FEED)) && result.data.content.feedID) {
-                        Location.goTo("#/feed/" + result.data.content.feedID);
-                        deferred.resolve();
-                      }
-                      else {
-                        Location.goTo("#/feed/1");
-                        deferred.resolve();
-                      }
-                    } else {
-                      Location.goTo("#/feed/1");
-                      deferred.resolve();
-                    }
-                  }
-                  , error = function (err) {
-                    Location.goTo("#/feed/1");
-                    deferred.reject();
-                  };
-                DataStore.get(TAG_NAMES.VIMEO_INFO).then(success, error);
-              }]
-          }
-        })
-        .when('/feed/:feedId', {
-          templateUrl: 'templates/home.html',
-          controllerAs: 'WidgetFeed',
-          controller: 'WidgetFeedCtrl'
+          template: '<div></div>'
         })
         .when('/video/:videoId', {
           templateUrl: 'templates/Item_Details.html',
           controller: 'WidgetSingleCtrl',
           controllerAs: 'WidgetSingle'
-
         })
         .otherwise('/');
     }])
@@ -122,13 +86,10 @@
     }])
     .run(['Location', '$location', '$rootScope', function (Location, $location, $rootScope) {
       buildfire.navigation.onBackButtonClick = function () {
-        var reg = /^\/feed/;
-        if ($rootScope.contentType == "Channel Feed") {
-          if (!($location.path().match(reg))) {
-            Location.goTo('#/');
-          } else {
-            buildfire.navigation.navigateHome();
-          }
+        var reg = /^\/video/;
+        if (reg.test($location.path()) && ($rootScope.contentType == "Channel Feed" || "User Feed")) {
+          $rootScope.showFeed = true;
+          Location.goTo('#/');
         }
         else {
           buildfire.navigation.navigateHome();
