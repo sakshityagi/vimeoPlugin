9bf2a2a310c8a4b57d26e915d9021c55c69efa38
lakshaydulani <lakshaydulani@gmail.com>
2016-05-05 15:32:44 +0530

    Carousel placeholder iphone fix

--- widget/controllers/widget.feed.controller.js
+++ widget/controllers/widget.feed.controller.js
@@ -19,7 +19,7 @@
 
         /*declare the device width heights*/
         $rootScope.deviceHeight = window.innerHeight;
-        $rootScope.deviceWidth = window.innerWidth;
+        $rootScope.deviceWidth = window.innerWidth || 320;
 
         /*
          * Fetch user's data from datastore
